5afb901156b06cada19ac5d498de31dbe8d00e63
Miralles <56624655+Junff97@users.noreply.github.com>
2021-02-03 20:28:00 +0800

    Update lc20.java

--- leecode/lc20.java
+++ leecode/lc20.java
@@ -1,34 +1,38 @@
 package leecode;
-
 import java.util.Stack;
 
 public class lc20 {
+	
+	//Methods of using the stack
 	public static boolean isValid(String s) {
-    	Stack<Character> heap = new Stack<>();
-    	char[] c = s.toCharArray();
-    	for (char ch : c) {
+    		Stack<Character> heap = new Stack<>();
+    		char[] c = s.toCharArray();
+    		for (char ch : c) {
+			
+			//Press the retrieved symbols that need to appear afterwards into the stack.
 			if (ch == '(') {heap.add(')');}
 			else if (ch =='[') {heap.add(']');}
 			else if (ch == '{') {heap.add('}');}
+			
+			//If the corresponding symbols retrieved are not in the order of pop out of the stack, an error is reported
 			else if (heap.isEmpty() || ch != heap.pop()) {return false;}
 		}
-    	return heap.isEmpty();
+		return heap.isEmpty();
+	}
+	
+	//Methods of string replacement
+	public static boolean isValid2(String s) {
+		int length = s.length() / 2;
+		for (int i = 0; i < length; i++) {
+			s = s.replace("()", "").replace("{}", "").replace("[]", "");
+		}
+		return s.length() == 0;
 	}
 	
 	public static void main(String[] args) {
-		//System.out.println(isValid("(]"));
+		System.out.println(isValid("(]"));
 		String s = "{()}()";
 		System.out.println(isValid2(s));
 	}
 	
-	
-	//第二种方法比较简单
-	 public static boolean isValid2(String s) {
-	        int length = s.length() / 2;
-			for (int i = 0; i < length; i++) {
-				s = s.replace("()", "").replace("{}", "").replace("[]", "");
-			}
-
-			return s.length() == 0;
-	    }
 }
