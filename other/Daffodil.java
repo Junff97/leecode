b7453ff80331456c9ecade37538fdd4e860826c6
Miralles <604056734@qq.com>
2021-02-16 21:46:29 +0800

    nothing

--- other/Daffodil.java
+++ other/Daffodil.java
@@ -1,4 +1,3 @@
-
 public class Daffodil {
 	public static void main(String[] args) {
 		int a1,a2,a3;
@@ -7,7 +6,7 @@ public class Daffodil {
 			a2 = i/10%10;
 			a3 = i%10;
 			if (Math.pow(a1, 3) + Math.pow(a2, 3) + Math.pow(a3, 3) == i) {
-				System.out.println("水仙花数为= " + i);
+				System.out.println("The number of daffodil is " + i);
 			}
 		}
 	}
