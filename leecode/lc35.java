c990127d4d7b75a2f83686609093290e2e3d971f
Miralles <604056734@qq.com>
2021-03-06 14:52:31 +0800

    dichotomy

--- leecode/lc35.java
+++ leecode/lc35.java
@@ -17,8 +17,23 @@ public class lc35 {
 		}return -1;
     }
 	
+	//¶þ·Ö·¨
+	public static int searchInsert2(int[] nums, int target) {
+		int n = nums.length;
+		int left = 0, right = n - 1, ans = n;
+		while(left <= right) {
+			int mid = (right-left)/2 + left;
+			if (nums[mid] >= target) {
+				right = mid - 1;
+				ans = mid; 
+			}else {
+				left = mid + 1;
+			}
+		}
+		return ans;
+	}
 	public static void main(String[] args) {
-		int[] res = {1,3,5,6};
-		System.out.println(searchInsert(res, 3));
+		int[] res = {1,3,5,9,12};
+		System.out.println(searchInsert2(res, 6));
 	}
 }
