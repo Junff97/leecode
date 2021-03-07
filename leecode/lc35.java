e22ea040cdacae0fb6f13742e84211d58cceb1d4
Miralles <604056734@qq.com>
2021-03-07 13:53:43 +0800

    Dichotomy

--- leecode/lc35.java
+++ leecode/lc35.java
@@ -17,23 +17,43 @@ public class lc35 {
 		}return -1;
     }
 	
-	//二分法
+	//二分法(left <= right)
 	public static int searchInsert2(int[] nums, int target) {
 		int n = nums.length;
-		int left = 0, right = n - 1, ans = n;
+		int left = 0, right = n - 1;
 		while(left <= right) {
 			int mid = (right-left)/2 + left;
-			if (nums[mid] >= target) {
-				right = mid - 1;
-				ans = mid; 
-			}else {
+			if (nums[mid] > target) {
+				right = mid - 1; 
+			}else if (nums[mid] < target) {
 				left = mid + 1;
+			}else{
+				return mid;
 			}
 		}
-		return ans;
+		return left;
 	}
+	
+	//二分法(left < right)
+	public static int searchInsert3(int[] nums, int target) {
+		int n = nums.length;
+		int left = 0, right = n;
+		while(left < right) {
+			int mid = (right-left)/2 + left;
+			if (nums[mid] > target) {
+				right = mid;
+			}else if(nums[mid] < target) {
+				left = mid + 1;
+			}else {
+				return mid;
+			}
+		}
+		return right;
+	}
+	
+	
 	public static void main(String[] args) {
-		int[] res = {1,3,5,9,12};
-		System.out.println(searchInsert2(res, 6));
+		int[] res = {1,3,5,6};
+		System.out.println(searchInsert2(res, 4));
 	}
 }
